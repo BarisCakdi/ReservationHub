dcc7f813a433cba4c62952f1dc443cfa00a48015
Baris <bariscakdi@gmail.com>
2024-10-01 15:16:04 +0300

    Entitiy ve model kurulum

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReservationHub.Models;
+
+namespace ReservationHub.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // fluent api yönetimi
+        {
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.User)  
+                .WithMany(u => u.Reservations)
+                .HasForeignKey(r => r.UserId);
+        }
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ReservationHub.Data;
+
 namespace ReservationHub
 {
     public class Program
@@ -8,6 +11,10 @@ namespace ReservationHub
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
 
             var app = builder.Build();
 
